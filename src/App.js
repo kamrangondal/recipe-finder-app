a943687effc3ccc5259a185172eb5a772f4b4498
Kamran Saif <kamran.gondal.747@gmail.com>
2023-03-13 17:50:01 +0500

    new-changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,431 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import FoodBankIcon from '@mui/icons-material/FoodBank';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+import GrassIcon from '@mui/icons-material/Grass';
+import SetMealIcon from '@mui/icons-material/SetMeal';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
-function App() {
+
+
+import Modal from '@mui/joy/Modal';
+import ModalClose from '@mui/joy/ModalClose';
+
+import Sheet from '@mui/joy/Sheet';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+
+  const [open, setOpen] = React.useState(false);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ flexGrow: 1, }}>
+      <AppBar position="static"
+        sx={{
+          background: 'radial-gradient(circle, #5c0067 0%, #00d4ff 100%)',
+
+        }}
+      >
+
+
+
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-desc"
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            maxWidth: 500,
+            borderRadius: 'md',
+            p: 3,
+            boxShadow: 'lg',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ModalClose
+            variant="outlined"
+            sx={{
+              top: 'calc(-1/4 * var(--IconButton-size))',
+              right: 'calc(-1/4 * var(--IconButton-size))',
+              boxShadow: '0 2px 12px 0 rgba(0 0 0 / 0.2)',
+              borderRadius: '50%',
+              bgcolor: 'background.body',
+            }}
+          />
+          {/* <Typography
+            component="h2"
+            id="modal-title"
+            level="h4"
+            textColor="inherit"
+            fontWeight="lg"
+            mb={1}
+          >
+            This is the modal title
+          </Typography>
+          <Typography id="modal-desc" textColor="text.tertiary">
+            Make sure to use <code>aria-labelledby</code> on the modal dialog with an
+            optional <code>aria-describedby</code> attribute.
+          </Typography> */}
+          <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea onClick={() => setOpen(true)}>
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      image="/ffg.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Biryani
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                      Behold! An authentic Chicken Biryani recipe with simple, easy-to-follow instructions (no curveballs!) and mouthwatering, traditional Pakistani and Indian flavor. This recipe includes tips on how to get fluffy rice, tender chicken, and the distinct biryani taste. Tested to perfection!
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                    Watch Tutorial
+                    </Button>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+        </Sheet>
+      </Modal>
+
+
+        <Toolbar>
+          <FoodBankIcon sx={{ display: { xs: 'none', md: 'flex', fontSize: 80 }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Recipes Bank
+          </Typography>
+          <Typography
+            variant="h5"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, textAlign: 'center', display: { xs: 'none', sm: 'block' } }}
+          >
+            Recipe Finder Application
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search by dish name or by ingredient…"
+              inputProps={{ 'aria-label': 'search' }}
+              sx={{ fontSize: 30 }}
+            />
+
+          </Search>
+        </Toolbar>
+      </AppBar>
+      <br />
+
+
+      {/* <section style={{paddingLeft: '20px', paddingRight: '20px'}}>
+
+        <Box>
+          <Typography variant="h4" component="div" gutterBottom>
+            Welcome to Recipes Bank
+          </Typography>
+        </Box>
+      </section> */}
+
+      <Grid container spacing={2}>
+        <Grid item xs={4} sm={3}>
+          <Stack spacing={2}>
+            <Item sx={{ boxShadow: 0 }}><Button><WidgetsIcon color="action" sx={{ marginRight: 2 }} />All recipes</Button></Item>
+            <Item sx={{ boxShadow: 0 }}><Button><GrassIcon color="action" sx={{ marginRight: 2 }} />Vegetarian</Button></Item>
+            <Item sx={{ boxShadow: 0 }}><Button><SetMealIcon color="action" sx={{ marginRight: 2 }} />Meat</Button></Item>
+            <Item sx={{ boxShadow: 0 }}><Button><FavoriteIcon color="action" sx={{ marginRight: 2 }} />Favourite</Button></Item>
+          </Stack>
+        </Grid>
+        <Grid item xs={8} sm={9}>
+          <Item sx={{ boxShadow: 0 }}>
+
+            <Grid container spacing={2}>
+
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea onClick={() => setOpen(true)}>
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      image="/ffg.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Biryani
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Behold! An authentic Chicken Biryani recipe...
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Watch Tutorial
+                    </Button>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+
+              </Grid>
+
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea onClick={() => setOpen(true)}>
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      image="/ffg.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Biryani
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Behold! An authentic Chicken Biryani recipe...
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                    Watch Tutorial
+                    </Button>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+
+              </Grid>
+
+
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea onClick={() => setOpen(true)}>
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      image="/ffg.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Biryani
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Behold! An authentic Chicken Biryani recipe...
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                    Watch Tutorial
+                    </Button>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+
+              </Grid>
+
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea onClick={() => setOpen(true)}>
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      image="/ffg.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Biryani
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Behold! An authentic Chicken Biryani recipe...
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                    Watch Tutorial
+                    </Button>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+
+              </Grid>
+
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea onClick={() => setOpen(true)}>
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      image="/ffg.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Biryani
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Behold! An authentic Chicken Biryani recipe...
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                    Watch Tutorial
+                    </Button>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  </CardActions>
+
+                </Card>
+
+              </Grid>
+
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea onClick={() => setOpen(true)}>
+                    <CardMedia
+                      component="img"
+                      height="180"
+                      image="/ffg.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Biryani
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Behold! An authentic Chicken Biryani recipe...
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Watch Tutorial
+                    </Button>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+
+              </Grid>
+
+            </Grid>
+
+          </Item>
+        </Grid>
+      </Grid>
+
+    </Box>
+
   );
 }
-
-export default App;
---
+++ src/recipes.js
@@ -0,0 +1,185 @@
+export const recipes = [{
+    id: 1,
+    name: 'Pancakes',
+    ingredients: [
+        '2 cups all-purpose flour',
+        '3 1/2 teaspoons baking powder',
+        '1 teaspoon salt',
+        '1 tablespoon white sugar',
+        '1 1/4 cups milk',
+        '1 egg',
+        '3 tablespoons butter, melted'
+    ],
+    instructions: [
+        'In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.',
+        'Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.'
+    ],
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Blueberry_pancakes_%283%29.jpg/1200px-Blueberry_pancakes_%283%29.jpg'
+}, {
+    id: 2,
+    name: 'Omelette',
+    ingredients: [
+        '4 eggs',
+        '2 tablespoons milk',
+        '1/4 teaspoon salt',
+        '1/8 teaspoon pepper',
+        '2 tablespoons butter',
+        '1/4 cup chopped onion',
+        '1/4 cup chopped green bell pepper',
+        '1/4 cup chopped fresh mushrooms',
+        '1/4 cup chopped fresh tomatoes',
+        '1/4 cup shredded Cheddar cheese'
+    ],
+    instructions: [
+        'In a medium bowl, beat the eggs, milk, salt and pepper. Set aside.',
+        'In a large skillet, melt butter over medium heat. Add onion, green pepper, mushrooms and tomatoes. Cook and stir until vegetables are tender. Pour egg mixture over vegetable mixture. As egg begins to set, gently push cooked portions from the edges toward the center with a spatula so that uncooked egg can reach the hot pan surface. Continue cooking, lifting and folding the omelet until the eggs are completely set. Sprinkle with cheese and serve.'
+    ],
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Blond_unbrowned_omelet_with_mushrooms_and_herbs.jpg/1200px-Blond_unbrowned_omelet_with_mushrooms_and_herbs.jpg'
+}, {
+    id: 3,
+    name: 'French Toast',
+    ingredients: [
+        '1/2 cup milk',
+        '2 eggs',
+        '1/2 teaspoon ground cinnamon',
+        '1/4 teaspoon ground nutmeg',
+        '1/4 teaspoon salt',
+        '1/2 teaspoon vanilla extract',
+        '4 slices white bread',
+        '1 tablespoon butter',
+        '1/4 cup maple syrup'
+    ],
+    instructions: [
+        'In a shallow bowl, mix together the milk, eggs, cinnamon, nutmeg, salt and vanilla. Place bread slices in the mixture to soak for 2 minutes on each side.',
+        'Heat a lightly oiled griddle or frying pan over medium high heat. Cook each side until golden brown. Serve hot with maple syrup.'
+    ],
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/FrenchToast.JPG/1200px-FrenchToast.JPG'
+}, {
+    id: 4,
+    name: 'Scrambled Eggs',
+    ingredients: [
+        '4 eggs',
+        '1/4 cup milk',
+        '1/4 teaspoon salt',
+        '1/8 teaspoon pepper',
+        '1 tablespoon butter'
+    ],
+    instructions: [
+        'In a medium bowl, beat the eggs, milk, salt and pepper. Set aside.',
+        'In a medium skillet, melt butter over medium heat. Pour in the eggs and scramble until cooked through. Serve hot.'
+    ],
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Scrambled_eggs.jpg'
+}, {
+    id: 5,
+    name: 'Bacon and Eggs',
+    ingredients: [
+        '4 slices bacon',
+        '4 eggs',
+        '1/4 teaspoon salt',
+        '1/8 teaspoon pepper'
+    ],
+    instructions: [
+        'In a large skillet, cook bacon over medium high heat until evenly brown. Drain, crumble and set aside.',
+        'In the same skillet, fry eggs in the bacon drippings until desired doneness. Season with salt and pepper. Serve eggs topped with crumbled bacon.'
+    ],
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Bacon_and_eggs.jpg'
+}, {
+    id: 6,
+    name: 'Hash Browns',
+    ingredients: [
+        '2 cups shredded potatoes',
+        '1/2 cup shredded onion',
+        '1/2 cup shredded cheddar cheese',
+        '1/2 teaspoon salt',
+        '1/4 teaspoon pepper',
+        '1/4 teaspoon dried fennel',
+        '1/4 teaspoon dried mint',
+        '1/4 teaspoon dried savory',
+        '1/4 teaspoon dried tarragon',
+        '1/4 teaspoon dried thyme',
+        '1/4 teaspoon dried winter savory',
+        '1/4 teaspoon dried chervil',
+        '1/4 teaspoon dried chives',
+        '1/4 teaspoon dried marjoram'
+    ],
+    instructions: [
+        'In a large bowl, mix together the potatoes, onion, cheese, salt, pepper and dried herbs. Form into patties.',
+        'Heat a lightly oiled skillet over medium high heat. Cook patties until golden brown on both sides.'
+    ],
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Hash_browns.jpg'
+}, {
+    id: 7,
+    name: 'Fried Rice',
+    ingredients: [
+        '2 cups cooked rice',
+        '1/2 cup frozen peas and carrots',
+        '1/2 cup chopped onion',
+        '1/2 cup chopped celery',
+        '1/2 cup chopped green bell pepper',
+        '1/2 cup chopped ham',
+    ],
+    instructions: [
+        'In a large skillet, saute the onion, celery and green pepper in oil until tender. Add the rice, peas and carrots, ham and soy sauce. Cook and stir until heated through.'
+    ],
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Fried_rice.jpg'
+}, {
+    id: 8,
+    name: 'Eggs Benedict',
+    ingredients: [
+        '4 English muffins',
+        '4 eggs',
+        '1/2 cup butter, melted',
+        '1/4 cup lemon juice',
+        '1/4 cup water',
+        '1/4 cup white sugar',
+        '1/4 teaspoon salt',
+        '1/4 teaspoon ground black pepper',
+        '1/4 teaspoon ground nutmeg',
+        '1/4 teaspoon ground cinnamon',
+        '1/4 teaspoon ground allspice',
+        '1/4 teaspoon ground cloves',
+        '1/4 teaspoon ground ginger',
+        '1/4 teaspoon ground mace',
+        '1/4 teaspoon ground cardamom',
+        '1/4 teaspoon ground coriander',
+        '1/4 teaspoon ground mustard',
+        '1/4 teaspoon ground cumin',
+        '1/4 teaspoon ground turmeric',
+        '1/4 teaspoon ground paprika',
+        '1/4 teaspoon ground fennel',
+        '1/4 teaspoon ground anise',
+        '1/4 teaspoon ground caraway',
+        '1/4 teaspoon ground dill',
+        '1/4 teaspoon ground sage',
+        '1/4 teaspoon ground rosemary',
+        '1/4 teaspoon ground thyme',
+        '1/4 teaspoon ground oregano',
+        '1/4 teaspoon ground basil',
+        '1/4 teaspoon ground tarragon',
+        '1/4 teaspoon ground marjoram',
+        '1/4 teaspoon ground savory',
+        '1/4 teaspoon ground winter savory',
+        '1/4 teaspoon ground chervil',
+        '1/4 teaspoon ground chives',
+        '1/4 teaspoon ground mint',
+        '1/4 teaspoon ground lavender',
+        '1/4 teaspoon ground bay leaf',
+        '1/4 teaspoon ground star anise',
+        '1/4 teaspoon ground celery seed',
+        '1/4 teaspoon ground cayenne pepper',
+        '1/4 teaspoon ground white pepper',
+        '1/4 teaspoon ground black pepper',
+        '1/4 teaspoon ground red pepper',
+        '1/4 teaspoon ground green pepper',
+        '1/4 teaspoon ground yellow pepper',
+    ],
+    instructions: [
+        'Preheat oven to 350 degrees F (175 degrees C). Lightly grease a 9x13 inch baking dish.',
+        'Place the English muffins in the prepared baking dish. In a small bowl, mix together the eggs, butter, lemon juice, water, sugar, salt and spices. Pour over the muffins.',
+        'Bake in the preheated oven for 20 to 25 minutes, or until the eggs are set.'
+    ],
+    img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Eggs_benedict.jpg'
+}
+
+
+]; // Array of recipes
